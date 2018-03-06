acabe1ebebb41538cc6f1c4a9272dcb4b5ffc81f
wzj <wzj@da4e1ff1-8fa2-7549-8234-3745f2c359fc>
2018-03-06 10:56:27 +0000

    git-svn-id: https://39.108.144.9:8443/svn/kjcphp@114 da4e1ff1-8fa2-7549-8234-3745f2c359fc
    Former-commit-id: 4765c0761077de2222cd9d1f3a7d245aca3aad45

--- 1.0.0/lib/function.php
+++ 1.0.0/lib/function.php
@@ -150,3 +150,88 @@ function logwrite($msg, $checkflag = 1)
        echo '{"error":true,"msg":"'.$tmsg.'"}';
        exit;  */
  }
+ /**
+ * @param $lat1
+ * @param $lng1
+ * @param $lat2
+ * @param $lng2
+ * @return int
+ */
+function getDistance($lat1, $lng1, $lat2, $lng2){
+
+    //将角度转为狐度
+
+    $radLat1=deg2rad($lat1);//deg2rad()函数将角度转换为弧度
+
+    $radLat2=deg2rad($lat2);
+
+    $radLng1=deg2rad($lng1);
+
+    $radLng2=deg2rad($lng2);
+
+    $a=$radLat1-$radLat2;
+
+    $b=$radLng1-$radLng2;
+
+    $s=2*asin(sqrt(pow(sin($a/2),2)+cos($radLat1)*cos($radLat2)*pow(sin($b/2),2)))*6378.137;
+
+    return $s;
+
+}
+ /**
+ * @param $lat1
+ * @param $lng1
+ * @param $lat2
+ * @param $lng2
+ * @param float $radius  星球半径
+ * @return float
+ */
+function distance($lat1, $lng1, $lat2,$lng2,$radius = EARTH_RADIUS)
+{
+    $rad = floatval(M_PI / 180.0);
+
+    $lat1 = floatval($lat1) * $rad;
+    $lng1 = floatval($lng1) * $rad;
+    $lat2 = floatval($lat2) * $rad;
+    $lng2 = floatval($lng2) * $rad;
+
+    $theta = $lng2 - $lng1;
+
+    $dist = acos(sin($lat1) * sin($lat2) +
+                cos($lat1) * cos($lat2) * cos($theta)
+            );
+
+    if ($dist < 0 ) {
+        $dist += M_PI;
+    }
+
+    return $dist = $dist * $radius;
+}
+/**
+* @desc 根据两点间的经纬度计算距离
+* @param float $lat 纬度值
+* @param float $lng 经度值
+*/
+function getDistance1($lat1, $lng1, $lat2, $lng2)
+{
+    $earthRadius = EARTH_RADIUS;
+
+    /*
+    Convert these degrees to radians
+    to work with the formula
+    */
+
+    $lat1 = ($lat1 * pi() ) / 180;
+    $lng1 = ($lng1 * pi() ) / 180;
+
+    $lat2 = ($lat2 * pi() ) / 180;
+    $lng2 = ($lng2 * pi() ) / 180;
+
+    $calcLongitude = $lng2 - $lng1;
+    $calcLatitude = $lat2 - $lat1;
+    $stepOne = pow(sin($calcLatitude / 2), 2) + cos($lat1) * cos($lat2) * pow(sin($calcLongitude / 2), 2);
+    $stepTwo = 2 * asin(min(1, sqrt($stepOne)));
+    $calculatedDistance = $earthRadius * $stepTwo;
+
+    return round($calculatedDistance);
+}
