a8b5e068b7796f47d7e2e1da5ffdc19caab5cd19
wzj <wzj@da4e1ff1-8fa2-7549-8234-3745f2c359fc>
2018-03-09 06:36:16 +0000

    git-svn-id: https://39.108.144.9:8443/svn/kjcphp@216 da4e1ff1-8fa2-7549-8234-3745f2c359fc
    Former-commit-id: 3aee66f2c14daf1618ac92da2a47c2a4fb224def

--- 1.0.0/orderReceiving.php
+++ 1.0.0/orderReceiving.php
@@ -1,5 +1,5 @@
 <?php
-    $hopedir = './';
+    $hopedir = '../';
     $config = $hopedir."config/hopeconfig.php";
     $cfg = include($config);
     $info = file_get_contents($cfg['siteurl']."/index.php?ctrl=site&action=orderReceiving");
