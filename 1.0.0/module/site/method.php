17a91fb85be7ee0d226d492f3d241be7ae9bd17c
wzj <wzj@da4e1ff1-8fa2-7549-8234-3745f2c359fc>
2018-03-26 06:27:24 +0000

    git-svn-id: https://39.108.144.9:8443/svn/kjcphp@447 da4e1ff1-8fa2-7549-8234-3745f2c359fc
    Former-commit-id: 6332511d306fd5cf322124b42b2938fa767df66c

--- 1.0.0/module/site/method.php
+++ 1.0.0/module/site/method.php
@@ -2291,7 +2291,7 @@ class method extends baseclass
         /* 更新订单物流信息 */
         $orderinfo = $this->mysql->select_one("select * from ".Mysite::$app->config['tablepre']."order where id = ".$orderid."   ");
         $minitime = strtotime(date('Y-m-d', time()));
-        $tj = $this->mysql->select_one("select daycode,id from ".Mysite::$app->config['tablepre']."order where shopid='".$orderinfo['shopid']."' and addtime > ".$minitime." AND paystatus = 1 order by id desc");
+        $tj = $this->mysql->select_one("select daycode,id from ".Mysite::$app->config['tablepre']."order where shopid='".$orderinfo['shopid']."' and addtime > ".$minitime." AND paystatus = 1 order by id desc limit 0,1000");
         $data['daycode'] = empty($tj)?1:$tj['daycode']+1;
         $this->mysql->update(Mysite::$app->config['tablepre'].'order',$data,"id ='".$orderid."' ");
         $orderCLs = new orderclass();
